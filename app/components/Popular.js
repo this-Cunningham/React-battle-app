787e07a50d9736cae10149eaa24b562ca0357664
Chris Cunningham <65561256+this-Cunningham@users.noreply.github.com>
2020-07-25 15:29:37 -0400

    Add components dir, added popular component

---
+++ app/components/Popular.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+  }
+
+  render(){
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <ul className = 'flex-center'>
+        {languages.map((language) => (
+          <li key = {language}>
+            <button className = 'btn-clear nav-link'>{language}</button>
+          </li>
+      ))}
+      </ul>
+    )
+  }
+}
