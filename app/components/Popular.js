e9e976d1082d4d2c53a1ddfd91d290712a43615a
Chris Cunningham <65561256+this-Cunningham@users.noreply.github.com>
2020-07-29 20:14:23 -0400

    Add ReposGrid w/HTML and Styling

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {fetchPopularRepos} from '../utils/api'
+import {FaUser, FaStar, FaCodeBranch, FaExclamationTriangle} from 'react-icons/fa'
 
 
 function NavLanguages ({selected, onUpdateLanguage}) {
@@ -9,16 +10,18 @@ function NavLanguages ({selected, onUpdateLanguage}) {
 
   return (
     <ul className = 'flex-center'>
-      {languages.map((language) => (
-        <li key = {language}>
-          <button
-            className = 'btn-clear nav-link'
-            style = {language === selected ? {color: 'green'}: null}
-            onClick = {()=> onUpdateLanguage(language)}>
-            {language}
-          </button>
-        </li>
-      ))}
+      {languages.map((language) => {
+        return (
+          <li key = {language}>
+            <button
+              className = 'btn-clear nav-link'
+              style = {language === selected ? {color: 'green'}: null}
+              onClick = {()=> onUpdateLanguage(language)}>
+              {language}
+            </button>
+          </li>
+        )
+      })}
     </ul>
   )
 }
@@ -28,6 +31,58 @@ NavLanguages.propTypes = {
   onUpdateLanguage: PropTypes.func.isRequired
 }
 
+//{repos} is our destructured prop that we will pass down from Popular Component=> this.props.repos, repos = repos[selectedLanguage] as we defined in Popular and it is an array
+function ReposGrid ({repos}) {
+  return (
+    <ul className='grid space-around'>
+      {repos.map((repo,index)=> {
+        const {name, owner, html_url, stargazers_count, forks, open_issues} = repo;
+        const {login, avatar_url} = owner;
+
+        return ( //must return to create new array from .map
+          <li key = {html_url} className='repo bg-light'>
+            <h4 className="header-lg center-text">
+              #{index+1}
+            </h4>
+            <img
+              className = 'avatar'
+              src={avatar_url}
+              alt={`Avatar for ${login}`}
+            />
+            <h2 className="center-text">
+              <a href={html_url} className="link">{login}</a>
+            </h2>
+            <ul className="card-list">
+              <li>
+                <FaUser color='rgb(255,191,116)' size = {22} />
+                <a href={`https://github.com/${login}`}>
+                {login}
+                </a>
+              </li>
+              <li>
+                <FaStar color = 'rgb(255,115, 0)' size = {22} />
+                {stargazers_count.toLocaleString()} stars
+              </li>
+              <li>
+                <FaCodeBranch color = 'rgb(129,195, 245)' size = {22} />
+                {forks.toLocaleString()} forks
+              </li>
+              <li>
+                <FaExclamationTriangle color = 'rgb(255,215, 0)' size = {22} />
+                {open_issues.toLocaleString()} open issues
+              </li>
+            </ul>
+          </li>
+        )
+
+      })}
+    </ul>
+  )
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
 
 export default class Popular extends React.Component {
   constructor(props) {
@@ -59,7 +114,7 @@ export default class Popular extends React.Component {
       .then((data)=> {
         this.setState(({repos})=> ({
           repos: {
-            ...repos,
+            ...repos,   //(ES2018) added spread properties to object literals. It copies own enumerable **properties** from a provided object onto a new object.
             [selectedLanguage]: data
           }
         }))
@@ -97,7 +152,7 @@ export default class Popular extends React.Component {
 
         {error && <p>{error}</p>}
 
-        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+        {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
 
       </React.Fragment>
 
